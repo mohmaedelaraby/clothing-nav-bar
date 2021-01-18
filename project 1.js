f6b862f94fba581a13623ce1678eb23829b4301b
mohamed mamdouh elaraby <48841407+mohmaedelaraby@users.noreply.github.com>
2021-01-19 01:54:45 +0200

    Add files via upload

---
+++ project 1.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    $("#drop-bu2").click(function(){
+        $("#con").slideToggle("slow");
+    });
+});
+
+$(document).ready(function(){
+    $("#dropdown-x1").click(function(){
+        $("#x1").slideToggle();
+    });
+});
+$(document).ready(function(){
+    $("#dropdown-x2").click(function(){
+        $("#x2").slideToggle();
+        
+    });
+});
+
+$(document).ready(function(){
+    $("#dropdown-x3").click(function(){
+        $("#x3").slideToggle();
+        
+    });
+});
+
